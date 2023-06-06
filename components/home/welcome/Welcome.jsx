ad49b5ab3a24f6fc4ef86a5ccc6aab5bef0cd7b1
Niraj Kumar <nirajk23523@gmail.com>
2023-06-06 22:24:49 +0530

    searchbar and scrollJobType

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -13,13 +13,64 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
+const jobTypes = [
+    "Full Time",
+    "Part Time",
+    "Contractor",
+    // "Freelance",
+    // "Intern",
+];
+
 const Welcome = () => {
     const router = useRouter();
+    const [activeJobType, setActiveJobType] = useState("Full Time");
+
     return (
         <View>
             <View style={styles.container}>
                 <Text style={styles.userName}>Hello, Niraj</Text>
                 <Text style={styles.welcomeMessage}>Find your perfect Job</Text>
+            </View>
+
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        value=""
+                        onChange={() => {}}
+                        placeholder="What are you looking for?"
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+                    <Image
+                        source={icons.search}
+                        resizeMode="contain"
+                        style={styles.searchBtnImage}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.tabsContainer}>
+                <FlatList
+                    horizontal
+                    data={jobTypes}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity
+                            style={styles.tab(activeJobType, item)}
+                            onPress={() => {
+                                setActiveJobType(item);
+                                router.push(`/search/${item}`);
+                            }}
+                        >
+                            <Text style={styles.tabText(activeJobType, item)}>
+                                {item}
+                            </Text>
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={(item) => item}
+                    contentContainerStyle={{ columnGap: SIZES.small }}
+                />
             </View>
         </View>
     );
