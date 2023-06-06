695fd7b927481eb890fb4958b1324ab40cb3c7f1
Niraj Kumar <nirajk23523@gmail.com>
2023-06-06 17:38:41 +0530

    changed header

--- app/index.js
+++ app/index.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+    NearbyJobs,
+    Popularjobs,
+    ScreenHeaderBtn,
+    Welcome,
+} from "../components";
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerTitle: "",
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.menu}
+                            deimension="60%"
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.profile}
+                            deimension="100%"
+                        />
+                    ),
+                }}
+            />
+        </SafeAreaView>
     );
 };
 
