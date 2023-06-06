c07a8e8bf0146e1c4813feacd34835f47f019849
Niraj Kumar <nirajk23523@gmail.com>
2023-06-06 23:18:29 +0530

    useFetch hook done

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { RAPID_API_KEY } from "@env";
+import { set } from "react-native-reanimated";
 
 const rapidApiKey = RAPID_API_KEY;
 // sometimes it doesn't load if not referenced soon, that's why it's here
@@ -19,4 +20,30 @@ const useFetch = (endpoint, query) => {
             "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
         },
     };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert("There is an api error");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    };
+
+    return { data, isLoading, error, refetch };
 };
