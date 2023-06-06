a284b3c3df773464711dc94be322126a93dc1437
Niraj Kumar <nirajk23523@gmail.com>
2023-06-06 22:44:58 +0530

    made basic popular card layout

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,9 +11,13 @@ import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
-import { PopularJobCard } from "../../common/cards/popular/PopularJobCard";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+    const router = useRouter();
+    const isLoading = false;
+    const err = false;
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -22,7 +26,23 @@ const Popularjobs = () => {
                     <Text style={styles.headerBtn}>Show all</Text>
                 </TouchableOpacity>
             </View>
-            <View style={styles.cardsContainer}></View>
+            <View style={styles.cardsContainer}>
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary} />
+                ) : err ? (
+                    <Text>Something went wrong</Text>
+                ) : (
+                    <FlatList
+                        data={[1, 2, 3, 4, 5, 6, 7, 8]}
+                        renderItem={({ item }) => (
+                            <PopularJobCard item={item} />
+                        )}
+                        keyExtractor={(item) => item?.job_id}
+                        contentContainerStyle={{ columnGap: SIZES.medium }}
+                        horizontal
+                    />
+                )}
+            </View>
         </View>
     );
 };
