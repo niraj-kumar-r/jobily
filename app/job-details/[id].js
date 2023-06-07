dff3d56ef999f0038c58d2058ce4fe35fbfba738
Niraj Kumar <nirajk23523@gmail.com>
2023-06-07 16:58:30 +0530

    added custome back button

---
+++ app/job-details/[id].js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useCallback, useState } from "react";
+import {
+    View,
+    Text,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+
+import {
+    Company,
+    JobAbout,
+    JobFooter,
+    JobTabs,
+    ScreenHeaderBtn,
+    Specifics,
+} from "../../components";
+import { COLORS, SIZES, icons } from "../../constants";
+import useFetch from "../../hooks/useFetch";
+
+const JobDetails = () => {
+    const params = useSearchParams();
+    const router = useRouter();
+
+    const { data, isLoading, error, refresh } = useFetch("job-details", {
+        job_id: params.id,
+    });
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()}
+                        />
+                    ),
+                }}
+            ></Stack.Screen>
+        </SafeAreaView>
+    );
+};
+
+export default JobDetails;
